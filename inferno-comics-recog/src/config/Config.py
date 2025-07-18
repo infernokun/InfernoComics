20831144db46ad88026bd54a576556b54af4f40a
InfernoKun <infernokun@outlook.com>
2025-07-17 20:24:40 -0400

    fix docker stuff and flask config

--- inferno-comics-recog/src/config/Config.py
+++ inferno-comics-recog/src/config/Config.py
@@ -4,7 +4,7 @@ class Config:
     API_VERSION = "v1"
     API_URL_PREFIX = f"/inferno-comics-recognition/api/{API_VERSION}"
     REST_API = os.getenv('REST_API', 'http://localhost:8080/inferno-comics-rest/api')
-    FLASK_HOST = os.getenv('API_HOST', 'localhost')
-    FLASK_PORT = int(os.getenv('API_PORT', 5000))
+    FLASK_HOST = os.getenv('RECOGNITION_HOST', 'localhost')
+    FLASK_PORT = int(os.getenv('RECOGNITION_PORT', 5000))
     FLASK_THREADS = int(os.getenv('API_THREADS', 4))
     FLASK_ENV = os.getenv('FLASK_ENV', 'development')
