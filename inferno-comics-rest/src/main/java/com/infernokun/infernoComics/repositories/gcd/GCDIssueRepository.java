e2eb8ed5b795c796f94db33b8006aebab782e538
Javoris Brown <jbrown@-icr-team.com>
2025-11-04 15:42:06 -0700

    version bumps and updates to web

--- inferno-comics-rest/src/main/java/com/infernokun/infernoComics/repositories/gcd/GCDIssueRepository.java
+++ inferno-comics-rest/src/main/java/com/infernokun/infernoComics/repositories/gcd/GCDIssueRepository.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-
 public interface GCDIssueRepository extends JpaRepository<GCDIssue, Long> {
     List<GCDIssue> findByPublicationDateContaining(String date);
     List<GCDIssue> findByTitleContainingIgnoreCase(String title);
